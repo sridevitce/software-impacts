8966b27b058958f674138fe24d8ce79aed2bde39
Dr S Sridevi <36024869+sridevitce@users.noreply.github.com>
2022-07-12 22:09:15 +0530

    Add files via upload

---
+++ Grid Search for algorithm Tuning and Ridge Regression.py
@@ -0,0 +1,50 @@
+# Grid Search for Algorithm Tuning
+# Diabetes Dataset
+from pandas import read_csv
+import numpy
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import GridSearchCV
+url = "https://goo.gl/bDdBiA"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+alphas = numpy.array([1,0.1,0.01,0.001,0.0001,0])
+param_grid = dict(alpha=alphas)
+model = Ridge()
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+grid.fit(X, Y)
+print(grid.best_score_)
+print(grid.best_estimator_.alpha)
+
+
+# In[24]:
+
+
+# RIDGE Regression algorithm On Diabetes Dataset
+from numpy import mean
+from numpy import std
+from numpy import absolute
+from pandas import read_csv
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import Ridge
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+# define model
+model = Ridge(alpha=1.0)
+# define model evaluation method
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+# evaluate model
+scores = cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+# force scores to be positive
+scores = absolute(scores)
+print('Mean MAE: %.3f (%.3f)' % (mean(scores), std(scores)))
+
+
+# In[ ]:
+
+
+
+
